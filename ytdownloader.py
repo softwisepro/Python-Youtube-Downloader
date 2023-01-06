6fbe58048c97eb1a447d3f6b043dec1ed18fe439
JaydenKweyunga <122126027+JaydenKweyunga@users.noreply.github.com>
2023-01-06 21:45:56 +0000

    Python Automation project

---
+++ ytdownloader.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print('Title: ', yt.title)
+
+print('views: ', yt.views)
+
+yt = yt.streams.get_lowest_resolution()
+
+yt.download('/workspaces/Python-Youtube-Downloader/Downloads')
